9c49b8200482ae0b71e7692f9b479a25c599bb2c
John E. Vincent <lusis.org+github.com@gmail.com>
2014-11-01 12:12:22 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer_email "dan.ryan@enstratius.com"
 license          "Apache 2.0"
 description      "Installs/Configures a Chef handler for reporting results to a Slack channel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.3"
 
 depends "chef_handler"
