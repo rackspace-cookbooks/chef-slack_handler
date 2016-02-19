9e981a304030b8efbb60f33f9a48606329843027
Martin Smith <martin@mbs3.org>
2016-02-18 17:18:39 -0800

    Release 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer_email "dan.ryan@enstratius.com"
 license          "Apache 2.0"
 description      "Installs/Configures a Chef handler for reporting results to a Slack channel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 depends "chef_handler"
